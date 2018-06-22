484fa0da4d7a833fe6b4b73e67c3c1f06f14259a
James Hickey <jamesmichaelhickey@gmail.com>
2018-06-22 13:00:20 -0300

    test for skipping sched. task with errors

--- Src/Tests/Scheduling/SchedulerOnErrorTests.cs
+++ Src/Tests/Scheduling/SchedulerOnErrorTests.cs
@@ -41,5 +41,33 @@ namespace Tests.Scheduling
             Assert.IsTrue(errorHandledCount == 2);
             Assert.IsTrue(successfulTaskCount == 4);
         }
+
+         [TestMethod]
+        public void TestSchedulerSkipsErrors()
+        {
+            int successfulTaskCount = 0;
+            var scheduler = new Scheduler();                      
+
+            void DummyTask()
+            {
+                successfulTaskCount++;
+                Console.Write("dummy");
+            };
+
+            void ThrowsErrorTask()
+            {
+                throw new Exception("dummy");
+            }
+
+            scheduler.Schedule(ThrowsErrorTask).EveryMinute();
+            scheduler.Schedule(DummyTask).EveryMinute();
+
+            scheduler.RunAt(DateTime.UtcNow);
+
+            Assert.IsTrue(successfulTaskCount == 1);
+        }
+
+
+        // add test for tasks with errors when theres no handler
     }
 }
