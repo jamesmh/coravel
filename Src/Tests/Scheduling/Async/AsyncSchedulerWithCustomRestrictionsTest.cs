61c2d8232991b31885de0eb5f05d8015799e0692
papakaliati <kongiann81@gmail.com>
2018-07-13 19:09:36 +0200

    Added CustomRestrictionsTest

---
+++ Src/Tests/Scheduling/Async/AsyncSchedulerWithCustomRestrictionsTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using Coravel.Scheduling.Schedule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Tests.Scheduling.Helpers.SchedulingTestHelpers;
+
+namespace Tests.Scheduling.Async
+{
+    [TestClass]
+    public class AsyncSchedulerWithCustomRestrictionsTest
+    {
+        [TestMethod]
+        public async Task AsyncSchedulesTest()
+        {
+            Contract.Ensures(Contract.Result<Task>() != null);
+            var scheduler = new Scheduler();
+            int taskRunCount = 0;
+
+            // Running async action synchrously to test warning
+            scheduler.Schedule(async () =>
+            {
+                await Task.Delay(1);
+            }).EveryMinute();
+
+            scheduler.ScheduleAsync(async () =>
+            {
+                await Task.Delay(1);
+                taskRunCount++;
+            }).EveryMinute();
+
+            scheduler.ScheduleAsync(async () =>
+            {
+                await Task.Delay(1);
+                taskRunCount++;
+            }).EveryMinute();
+
+            scheduler.ScheduleAsync(async () =>
+            {
+                await Task.Delay(1);
+                taskRunCount++;
+            }, () => taskRunCount < 2)
+                     .EveryMinute();
+
+            scheduler.ScheduleAsync(async () =>
+            {
+                await Task.Delay(1);
+                taskRunCount++;
+            }, () => taskRunCount == 2)
+                     .EveryMinute();
+
+            scheduler.ScheduleAsync(async () =>
+            {
+                await Task.Delay(1);
+                taskRunCount++;
+            }, () => taskRunCount == 3)
+                     .EveryMinute();
+
+            await RunScheduledTasksFromMinutes(scheduler, 0);
+
+            Assert.AreEqual(4, taskRunCount);
+        }
+    }
+}
