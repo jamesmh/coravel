bb58b0bacac6ede801a8952ab83eec3c6a871390
James Hickey <jamesmichaelhickey@gmail.com>
2018-08-24 09:41:14 -0300

    mutex tests

--- Src/UnitTests/Scheduling/Mutex/InMemoryMutexTests.cs
+++ Src/UnitTests/Scheduling/Mutex/InMemoryMutexTests.cs
@@ -1,7 +1,32 @@
+using System.Threading.Tasks;
+using Coravel.Scheduling.Schedule.Interfaces;
+using Coravel.Scheduling.Schedule.Mutex;
+using Xunit;
+
 namespace UnitTests.Scheduling.Mutex
 {
     public class InMemoryMutexTests
     {
-        
+        [Fact]
+        public void InMemoryMutexLocks() {
+            IMutex  mutex = new InMemoryMutex();
+            string key = "mutexkey";
+            string otherKey = "anotherkey";
+            int timeout_24hours = 1440;
+
+            bool firstTry = mutex.TryGetLock(key, timeout_24hours);
+            bool secondTry = mutex.TryGetLock(key, timeout_24hours);
+
+            bool lockOtherKey = mutex.TryGetLock(otherKey, timeout_24hours);
+
+            mutex.Release(key);
+
+            bool thirdTry = mutex.TryGetLock(key, timeout_24hours);
+
+            Assert.True(firstTry);
+            Assert.False(secondTry);
+            Assert.True(lockOtherKey);
+            Assert.True(thirdTry);
+        }
     }
 }
