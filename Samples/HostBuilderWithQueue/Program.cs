805de3ca6f43729ce80e5a7e096f0424beb53ff2
James Hickey <jamesmichaelhickey@gmail.com>
2018-09-20 12:40:07 -0300

    HostBuilder sample

---
+++ Samples/HostBuilderWithQueue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Coravel.Queuing.Interfaces;
+using Coravel;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace HostBuilderWithQueue
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configApp.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddQueue();
+                    services.AddScheduler();
+                })        
+                .UseConsoleLifetime()        
+                .Build();
+
+            host.Services.GetRequiredService<IQueue>().QueueAsyncTask(async () =>
+            {
+                await Task.Delay(1000);
+                Console.WriteLine("This was queued.");
+            });
+
+            host.Services.UseScheduler(s =>
+                s.Schedule(() => Console.WriteLine("This was scheduled.")).EveryMinute()
+            );
+
+            await host.StartAsync();
+
+            Console.WriteLine("This runs for five minutes then shuts down.");
+            await Task.Delay(60000 * 5);
+
+            await host.StopAsync();
+        }
+    }
+}
