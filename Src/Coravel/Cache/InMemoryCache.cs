ade435a1b46c38ec3d7c001e44fdc87a5da4104f
Seymour Poler <seymourpoler@gmail.com>
2018-11-10 15:04:05 +0100

    clean using header

--- Src/Coravel/Cache/InMemoryCache.cs
+++ Src/Coravel/Cache/InMemoryCache.cs
@@ -1,13 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq.Expressions;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using Coravel.Cache.Interfaces;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Logging;
 
 namespace Coravel.Cache
 {
--- Src/IntegrationTests/TestMvcApp/Startup.cs
+++ Src/IntegrationTests/TestMvcApp/Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Coravel;
+﻿using Coravel;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TestMvcApp.Models;
 
 namespace TestMvcApp
 {
