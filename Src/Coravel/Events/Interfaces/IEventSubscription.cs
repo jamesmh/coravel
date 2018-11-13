50a6c7eb5e562858f412768d7cdc6ae435e1e153
Luis Rodríguez <seymourpoler@hotmail.com>
2018-11-13 15:42:29 +0100

    adding the summary

--- Src/Coravel/Events/Interfaces/IEventSubscription.cs
+++ Src/Coravel/Events/Interfaces/IEventSubscription.cs
@@ -2,6 +2,11 @@ namespace Coravel.Events.Interfaces
 {
     public interface IEventSubscription<TEvent> where TEvent : IEvent
     {
+        /// <summary>
+        /// Subscribe to a new listener for an event
+        /// </summary>
+        /// <typeparam name="TListener"></typeparam>
+        /// <returns></returns>
         IEventSubscription<TEvent> Subscribe<TListener>() where TListener : IListener<TEvent>;
     }
 }
