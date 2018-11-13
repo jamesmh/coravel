32bcd64fcba33a2cc8942c79d4bc79036160e623
Luis Rodríguez <seymourpoler@hotmail.com>
2018-11-13 15:45:31 +0100

    adding summary

--- Src/Coravel/Events/Interfaces/IListener.cs
+++ Src/Coravel/Events/Interfaces/IListener.cs
@@ -4,6 +4,11 @@ namespace Coravel.Events.Interfaces
 {
     public interface IListener<TEvent> where TEvent : IEvent
     {
+        /// <summary>
+        /// Handles an event
+        /// </summary>
+        /// <param name="broadcasted"></param>
+        /// <returns></returns>
         Task HandleAsync(TEvent broadcasted);
     }
 }
