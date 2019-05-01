b976ea886172fec93989a5ac9392cd86a4e96e3c
James Hickey <jamesmichaelhickey@gmail.com>
2019-05-01 13:39:26 -0300

    efcore sample addded

---
+++ Samples/EFCoreSample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Coravel.Queuing.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using test.Invocables;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IQueue _queue;
+
+        public HomeController(IQueue queue)
+        {
+            this._queue = queue;
+        }
+
+        public IActionResult Index()
+        {
+            this._queue.QueueInvocable<TestInvocable>();
+            this._queue.QueueInvocable<TestInvocable>();
+            this._queue.QueueInvocable<TestInvocable>();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Samples/EFCoreSample/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace test.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<TestModel> Test { get; set; }
+    }
+}
