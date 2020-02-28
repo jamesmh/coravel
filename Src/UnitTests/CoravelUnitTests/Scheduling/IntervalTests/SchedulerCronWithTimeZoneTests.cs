eeca2fec2977b5f93cf4546e555fd46d132865f6
Christian Schmitt <c.schmitt@envisia.de>
2020-02-28 15:36:28 +0100

    remove unnecessary imports

--- Src/UnitTests/CoravelUnitTests/Scheduling/IntervalTests/SchedulerCronWithTimeZoneTests.cs
+++ Src/UnitTests/CoravelUnitTests/Scheduling/IntervalTests/SchedulerCronWithTimeZoneTests.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Globalization;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Coravel.Scheduling.Schedule;
 using Coravel.Scheduling.Schedule.Mutex;
