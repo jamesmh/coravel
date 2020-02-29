ddc5d641d936db9f75cab181d3e29eb3e8d96ac4
Emily Myrestam <emily.myrestam@publicis.sapient.com>
2020-02-29 14:51:16 +0100

    Added docs to public extension methods

--- Src/Coravel/EventServiceRegistration.cs
+++ Src/Coravel/EventServiceRegistration.cs
@@ -27,11 +27,24 @@ namespace Coravel
             throw new Exception("Coravel.ConfigureEvents has been mis-configured.");
         }
 
+
+        /// <summary>
+        /// Registers all Coravel events and listers from the assembly containing the type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddEventsFromAssembly<T>(this IServiceCollection services)
         {
             return services.AddEventsFromAssembly(typeof(T).Assembly);
         }
 
+        /// <summary>
+        /// Registers all Coravel events and listeners from the given assembly.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
         public static IServiceCollection AddEventsFromAssembly(this IServiceCollection services, Assembly assembly)
         {
             var assemblyScanner = new AssemblyScanner(services);
@@ -42,6 +55,12 @@ namespace Coravel
             return services;
         }
 
+        /// <summary>
+        /// Registers all Coravel events and listeners from the given assemblies.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assemblies"></param>
+        /// <returns></returns>
         public static IServiceCollection AddEventsFromAssemblies(this IServiceCollection services, params Assembly[] assemblies)
         {
             var assemblyScanner = new AssemblyScanner(services);
@@ -55,6 +74,11 @@ namespace Coravel
             return services;
         }
 
+        /// <summary>
+        /// Subscribes all listeners to their corresponding events.
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <returns></returns>
         public static IServiceProvider UseCoravelEvents(this IServiceProvider provider)
         {
             var dispatcher = provider.GetRequiredService<IDispatcher>();
