cf3258bf896e81e45bcb496d5234a5acdaccd600
James Hickey <jamesmh@users.noreply.github.com>
2021-09-07 23:19:07 -0300

    cron ranges with divisors working (#253)

--- Src/Coravel/Scheduling/Schedule/Cron/CronExpressionPart.cs
+++ Src/Coravel/Scheduling/Schedule/Cron/CronExpressionPart.cs
@@ -46,7 +46,7 @@ namespace Coravel.Scheduling.Schedule.Cron
                 return true;
             }
 
-            bool isDivisibleUnit = expression.IndexOf("*/") > -1;
+            var isDivisibleUnit = expression.IndexOf("*/") > -1;
 
             if (isDivisibleUnit)
             {
@@ -69,107 +69,7 @@ namespace Coravel.Scheduling.Schedule.Cron
             }
             else
             {
-                var values = this.GetCronIntArray(expression);
-                return values.Contains(time);
-            }
-        }
-
-        /// <summary>
-        /// From the cron expression, get all the int values that are valid due times.
-        /// </summary>
-        /// <param name="expression"></param>
-        /// <returns></returns>
-        private IEnumerable<int> GetCronIntArray(string expression)
-        {
-            bool isRange = expression.IndexOf('-') > -1;
-            bool isDelineatedArray = expression.IndexOf(',') > -1;
-
-            if (isRange && isDelineatedArray)
-            {
-                throw new Exception($"Cron expression '{expression}' has mixed entry type.");
-            }
-
-            if (isRange)
-            {
-                return this.GetRange(expression);
-            }
-            else if (isDelineatedArray)
-            {
-                return this.GetDelineatedArray(expression);
-            }
-            else
-            {
-                return GetSpecifiedInt(expression);
-            }
-        }
-
-        /// <summary>
-        /// Get value from a cron expression with a single value.
-        /// </summary>
-        /// <param name="expression"></param>
-        /// <returns></returns>
-        private static IEnumerable<int> GetSpecifiedInt(string expression)
-        {
-            bool parsed = int.TryParse(expression, out var parsedValue);
-
-            if (!parsed)
-            {
-                throw new Exception($"Cron entry '{expression}' is malformed.");
-            }
-
-            return new int[] { parsedValue };
-        }
-
-        /// <summary>
-        /// Get values from cron expression like '5,4,3'.
-        /// </summary>
-        /// <param name="expression"></param>
-        /// <returns></returns>
-        private IEnumerable<int> GetDelineatedArray(string expression)
-        {
-            var delineatedValues = expression.Split(',');
-            foreach (var val in delineatedValues)
-            {
-                if (!int.TryParse(val, out var parsedValue))
-                {
-                    throw new Exception($"Cron entry '{expression}' is malformed.");
-                }
-
-                yield return parsedValue;
-            }
-        }
-
-        /// <summary>
-        /// Get values from cron expressio range (e.g.true '5-10').
-        /// </summary>
-        /// <param name="expression"></param>
-        /// <returns></returns>
-        private IEnumerable<int> GetRange(string expression)
-        {
-            // e.g. "5-10"
-            var range = expression.Split('-');
-            bool firstParsed = int.TryParse(range[0], out var first);
-            bool secondParsed = int.TryParse(range[1], out var second);
-
-            if (!(firstParsed && secondParsed))
-            {
-                throw new Exception($"Cron expression ${expression} is malformed.");
-            }
-
-            return this.Between(first, second);
-        }
-
-        /// <summary>
-        /// Get all int between (including) first and second.
-        /// </summary>
-        /// <param name="first"></param>
-        /// <param name="second"></param>
-        /// <returns></returns>
-        private IEnumerable<int> Between(int first, int second)
-        {
-            for (int i = first; i <= second; i++)
-            {
-                yield return i;
+                return new CronExpressionComplexPart(expression).CheckIfTimeIsDue(time);
             }
         }
     }
